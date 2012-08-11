47b1331f7643cd38d931a6253fc9a3a91dab5f13
fakestarbaby <fakestarbaby@gmail.com>
2012-08-11 18:51:42 +0900

    Added rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleGeocoder::Application.config.secret_token = 'c71cad2a0470fbfa075ee2ce6f791cf8d36641eef326302f2af93552ce4670399ea0be92a74760ab960b195932c0d69438fe9623e5232c27ec4a93c5e696e62b'
